57b02df65726e6fd4469c5ba55f2fd50f0d1a0ca
Jessicawu24 <44040575+Jessicawu24@users.noreply.github.com>
2019-02-13 17:04:42 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,11 @@
+$("button").click(function(){
+    var input1= $("#answer1").val();
+    var input2= $("#answer2").val();
+    var input3= $("#answer3").val();
+    var input1=input1*2.34751;
+    input2=input2*2;
+    input3=input3*2;
+$(".result").append("<p>You are " + input1 + " years old in " + 4717 + ", You watch the news " + input2 + " times, You encounter DEATH " + input3 + " times since " + " your birth, You will die on 4719." + "</p>" );
+});
+
+    
